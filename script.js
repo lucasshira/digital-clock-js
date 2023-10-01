4968bd46a6f2391646db916d4a2cf4ea5dd10fbe
Lucas Gabriel <144144880+lucasshira@users.noreply.github.com>
2023-10-01 12:27:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const getClock = () => {
     const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
     
     const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-    const formattedDate = `${day}/${month}/${year}`;
+    const formattedDate = `${day < 10 ? '0' : ''}${day}/${month}/${year}`;
     const fullDate = `${dayOfWeek}, ${formattedDate}`;
     
     clock.textContent = formattedTime;
